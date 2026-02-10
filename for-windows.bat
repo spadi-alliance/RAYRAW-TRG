528ebc06624a05b244c310e5a26c5582ffcfdcf0
Ryotaro Honda <rhonda@post.kek.jp>
2026-02-10 19:31:42 +0900

    for rayraw-v1

--- for-windows.bat
+++
@@ -1,2 +0,0 @@
-set vivado_path=%1
-%vivado_path% -mode tcl -nolog -nojournal -source .\project.tcl -tclargs --origin_dir .
--- user-setting.tcl
+++ user-setting.tcl
@@ -3,8 +3,8 @@ set_property ip_repo_paths $origin_dir/ip [current_project]
 update_ip_catalog
 set_param project.defaultIPCacheSetting none
 
-file mkdir $origin_dir/build/temp_constraints
-close [ open $origin_dir/build/temp_constraints/debug.xdc w ]
-read_xdc $origin_dir/build/temp_constraints/debug.xdc
+file mkdir $origin_dir/$build_dir/temp_constraints
+close [ open $origin_dir/$build_dir/temp_constraints/debug.xdc w ]
+read_xdc $origin_dir/$build_dir/temp_constraints/debug.xdc
 set_property TARGET_CONSTRS_FILE [get_files debug.xdc] [get_filesets constrs_1]
 
